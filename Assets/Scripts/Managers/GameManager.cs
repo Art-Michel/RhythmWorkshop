66399c46b005d5355ea1bf8e66a7e6a2430eab2a
Kitsuto <kitsuto.game@hotmail.com>
2021-12-13 16:35:31 +0100

    SceneClement GameManager Music and animTimeline

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class GameManager : LocalManager<GameManager>
+{
+    [SerializeField] private AudioClip music;
+    [SerializeField] private AudioSource musicSource;
+
+    private void Start() 
+    {
+        musicSource.clip = music;    
+    }
+
+    [Button]
+    void PlayMusic()
+    {
+        musicSource.Play();
+    }
+
+    [Button]
+    void PauseMusic()
+    {
+        musicSource.Pause();
+    }
+}
