c1df3c5752f5cc4879814e1226d7415011d21c47
Bino46 <atomemeteore@gmail.com>
2021-12-14 17:52:29 +0100

    Barre de vie à la dark souls

--- Assets/Scripts/Boss/BossComportement/BossHp.cs
+++ Assets/Scripts/Boss/BossComportement/BossHp.cs
@@ -6,24 +6,53 @@ using UnityEngine.UI;
 public class BossHp : LocalManager<BossHp>
 {
     public float BossHealthPoints { get; private set; }
+    public float oldBossHealthPoints;
+    [SerializeField] float speed;
+    float chrono = 0;
+    public bool tookDamage;
+
     [SerializeField] float _maxHP = 300;
 
     [SerializeField] Image _hpBar;
+    [SerializeField] Image _hpDamageBar;
 
     private void Start()
     {
         BossHealthPoints = _maxHP;
+        oldBossHealthPoints = BossHealthPoints;
     }
 
     public void TakeDamage(float damageAmount)
     {
         BossHealthPoints -= damageAmount;
         UpdateHPBar();
+        tookDamage = true;
         SoundManager.Instance.PlaybossGruntLight();
     }
 
     private void UpdateHPBar()
     {
         _hpBar.fillAmount = BossHealthPoints / _maxHP;
+    }
+
+    private void Update()
+    {
+        if (tookDamage == true)
+        {
+            chrono += Time.deltaTime;
+            if (chrono >= 0.5f)
+            {
+                chrono=0f;
+                tookDamage = false;
+            }
+        }
+
+        if (tookDamage == false)
+        {
+            float timer=0f;
+            timer+=Time.deltaTime;
+            oldBossHealthPoints = Mathf.Lerp(oldBossHealthPoints, BossHealthPoints, timer);
+            _hpDamageBar.fillAmount = oldBossHealthPoints / _maxHP;
+        }
     }
 }
