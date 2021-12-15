6796d9818b175f0342b0d7c3fd6b536db88f3c69
Bino46 <atomemeteore@gmail.com>
2021-12-15 21:31:10 +0100

    stuff

--- Assets/Scripts/Boss/BossComportement/BossComportement.cs
+++ Assets/Scripts/Boss/BossComportement/BossComportement.cs
@@ -96,7 +96,7 @@ public class BossComportement : MonoBehaviour
                 case 1:
                 case 2:
                 case 3:
-                    circleAttack.numberOfProjectiles = 7;
+                    //circleAttack.numberOfProjectiles = 7;
                     StartCoroutine(Circle());
                     maxDelayAttack -= difficultyAdd;
                     break;
@@ -145,7 +145,6 @@ public class BossComportement : MonoBehaviour
 
     IEnumerator Spirale()
     {
-        delayAttack -= maxDelayAttack / 4;
         spiraleAttack.Attack();
 
         yield return new WaitForSeconds(delayAttack);
