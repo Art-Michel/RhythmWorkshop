937984f7fccb9a7350acf2e007c44cb29cfff3eb
Arthur MSI <arthur.michel@e-artsup.net>
2021-12-17 09:38:37 +0100

    + lisible notes

--- Assets/Scripts/Notes/PreNote.cs
+++ Assets/Scripts/Notes/PreNote.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PreNote : MonoBehaviour
 {
     float _t = 0;
     Pool _pool;
 
-   public void InitializePreNote(Transform button, Pool pool)
+    SpriteRenderer _spriteRenderer;
+    [SerializeField] Sprite _prenoteNorthSprite;
+    [SerializeField] Sprite _prenoteSouthSprite;
+    [SerializeField] Sprite _prenoteEastSprite;
+    [SerializeField] Sprite _prenoteWestSprite;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void InitializePreNote(Transform button, Pool pool)
     {
         this._pool = pool;
         _t = 0;
@@ -15,13 +27,34 @@ public class PreNote : MonoBehaviour
         transform.localPosition = Vector3.zero;
         transform.localScale = Vector3.zero;
 
+        switch (button.tag)
+        {
+            case "South":
+                _spriteRenderer.sprite = _prenoteSouthSprite;
+                break;
+
+            case "North":
+                _spriteRenderer.sprite = _prenoteNorthSprite;
+                break;
+
+            case "East":
+                _spriteRenderer.sprite = _prenoteEastSprite;
+                break;
+
+            case "West":
+                _spriteRenderer.sprite = _prenoteWestSprite;
+                break;
+        }
+
     }
+
+
     void Update()
     {
         _t += Time.deltaTime * 0.69f;
         float scale = Mathf.Lerp(0.75f, 0.3f, _t);
-        transform.localScale = new Vector3(scale,scale,scale);
-        if (_t >=1)
+        transform.localScale = new Vector3(scale, scale, scale);
+        if (_t >= 1)
         {
             _pool.Back(gameObject);
         }
